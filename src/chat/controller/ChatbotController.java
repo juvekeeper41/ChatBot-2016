9fb81271dbde4a0b998963875b1d083199a3d93f
juvekeeper41 <juvekeeper41@gmail.com>
2016-10-18 08:57:58 -0600

    Added popup method to ask question and lengthChecker to ensure validity of the response.

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,10 +1,28 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import chat.view.ChatViewer;
+
 public class ChatbotController
 {
+	private Chatbot stupidBot;
+	private ChatViewer display;
+	
+	public ChatbotController()
+	{
+		stupidBot = new Chatbot("Unintelligent Chat Machine");
+		display = new ChatViewer();
+	}
+	
 	public void start()
 	{
+		String response = "words";
 		
+		while(stupidBot.lengthChecker(response))
+		{
+			
+			response = display.collectResponse("What do you want to talk about?");
+		}
 	}
 
 }
