6abb3f50671e997a25827d1b2609037662f9d5dd
juvekeeper41 <josb6385@740s-pl-114.local>
2017-03-17 08:03:10 -0600

    Built the FileController class.

---
+++ src/chat/controller/FileController.java
@@ -0,0 +1,37 @@
+package chat.controller;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class FileController 
+{
+	public static void saveFile(ChatController baseController, String fileName, String contents)
+	{
+		try
+		{
+			File saveFile;
+			if(fileName.length() > 5)
+			{
+				saveFile = new File(fileName + ".txt");
+			}
+			else
+			{
+				String saveName = LocalDateTime.now().getDayOfWeek().name() + " ";
+				saveName += LocalDateTime.now().getHour() + ":" + LocalDateTime.now().getMinute();
+				saveFile = new File(saveName);
+			}
+			FileWriter saveFileWriter = new FileWriter(saveFile);
+			saveFileWriter.write(contents);
+			saveFileWriter.close();
+			JOptionPane.showMessageDialog(baseController.getBaseFrame(), "Chat saved successfully as " + saveFile + " !");
+		}
+		catch(IOException error)
+		{
+			baseController.handleErrors(error);
+		}
+	}
+	
+}
